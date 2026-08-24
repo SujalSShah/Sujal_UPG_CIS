21411c68ee950f4c30993ddfab00c42af1b70a15
Sujal Shah <84385401+SujalSShah@users.noreply.github.com>
2026-08-24 10:24:51 +0530

    Update P7.py

--- Code/P7.py
+++ Code/P7.py
@@ -4,27 +4,27 @@ print("A Hindi Word is शक्ति")
 for char in text:
     if char == '\u094D': # Unicode for Virama
         print("Virama found!n")
-    else:
-        print(" No Virama found")
+ #   else:
+ #       print(" No Virama found")
 
 
 
-print("\nExample 2")
-text = "कमल"
-print("A Hindi Word is कमल")
-for char in text:
-    if char == '\u094D':
-        print("Virama found")
-    else:
-        print(" No Virama found")
+#print("\nExample 2")
+#text = "कमल"
+#print("A Hindi Word is कमल")
+#for char in text:
+#    if char == '\u094D':
+#        print("Virama found")
+#    else:
+#        print(" No Virama found")
 
 
 
-print("\nExample 3")
-text = "શબ્દ"
-print("A Gujarati Word is શબ્દ")
-for char in text:
-    if char == '\u0ACD':
-        print("Virama found!")
-    else:
-        print(" No Virama found")
+#print("\nExample 3")
+#text = "શબ્દ"
+#print("A Gujarati Word is શબ્દ")
+#for char in text:
+#    if char == '\u0ACD':
+#        print("Virama found!")
+#    else:
+#        print(" No Virama found")
